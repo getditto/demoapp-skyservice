16e62d40332af3e9cb34ac5ec29e1053a12e8996
Fuad Kamal <fuad@anaara.com>
2023-05-05 11:47:09 -0400

    refactor
    put class in its own classfile

--- Android/app/src/main/java/live/dittolive/skyservice/SkyServiceApplication.kt
+++ Android/app/src/main/java/live/dittolive/skyservice/SkyServiceApplication.kt
@@ -62,19 +62,3 @@ class SkyServiceApplication: Application() {
         notificationManager.createNotificationChannel(channel)
     }
 }
-class AuthCallback: DittoAuthenticationCallback {
-    override fun authenticationRequired(authenticator: DittoAuthenticator) {
-        authenticator.loginWithToken(BuildConfig.DITTO_AUTH_TOKEN, BuildConfig.DITTO_AUTH_PROVIDER) { err ->
-            println("Login request completed. Error? $err")
-        }
-    }
-
-    override fun authenticationExpiringSoon(
-        authenticator: DittoAuthenticator,
-        secondsRemaining: Long
-    ) {
-        authenticator.loginWithToken(BuildConfig.DITTO_AUTH_TOKEN, BuildConfig.DITTO_AUTH_PROVIDER) { err ->
-            println("Login request completed. Error? $err")
-        }
-    }
-}
